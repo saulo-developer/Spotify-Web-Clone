3de4d883a35c86ccfc9f44ab654b1c6174d042cd
Saulo Ribeiro <saudetarso@hotmail.com>
2024-01-06 11:03:19 -0300

    updating the design front 2

--- src/app/component/song-card/song-card.component.ts
+++ src/app/component/song-card/song-card.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-song-card',
   templateUrl: './song-card.component.html',
-  styleUrls: ['./song-card.component.scss']
+  styleUrls: ['./song-card.component.css']
 })
 export class SongCardComponent {
 
