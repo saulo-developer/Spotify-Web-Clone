96ac7e0102fc11b47cc077ed7fe2e6f934ae84bd
Saulo Ribeiro <saudetarso@hotmail.com>
2024-01-06 20:27:20 -0300

    updating the design front 3

--- src/app/component/song-card/song-card.component.ts
+++ src/app/component/song-card/song-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-song-card',
   templateUrl: './song-card.component.html',
   styleUrls: ['./song-card.component.css']
 })
-export class SongCardComponent {
+export class SongCardComponent implements OnInit{
+
+  @Input() public playlistThumbnail!: string;
+  @Input() public title!: string;
+  @Input() public description!: string;
+
+  constructor() {}
+
+  ngOnInit(): void {}
 
 }
